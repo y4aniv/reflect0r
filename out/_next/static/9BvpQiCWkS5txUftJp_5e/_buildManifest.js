5b973f7d3b8a50ee8b683acaced4f4e6e6cf9f0b
Yaniv <120222659+y4aniv@users.noreply.github.com>
2023-04-13 20:26:45 +0000

    build

---
+++ out/_next/static/9BvpQiCWkS5txUftJp_5e/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/af69b8ad71d8b8b4.css","static/chunks/pages/index-09b35d5d68189035.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
